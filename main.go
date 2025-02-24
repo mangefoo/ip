a673ac4ed0d97bc6fd436444216f1705040ec55a
Magnus Eriksson <eriksson.mag@gmail.com>
2025-02-24 21:24:17 +0100

    Change endpoints

--- main.go
+++ main.go
@@ -98,8 +98,8 @@ func ipHandlerPlainText(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/ip", ipHandlerHTML)
-	http.HandleFunc("/ip/plain", ipHandlerPlainText)
+	http.HandleFunc("/", ipHandlerHTML)
+	http.HandleFunc("/ip", ipHandlerPlainText)
 	fmt.Println("Server starting on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
